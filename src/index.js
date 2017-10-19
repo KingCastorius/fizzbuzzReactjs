4eeb3060430aa5545410699a3a16374ece1f16ef
KingCastorius <malizakzorz@gmail.com>
2017-10-19 16:19:42 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class FizzBuzz extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 1,
+      fizzBuzz: ''
+    }
+  }
+  
+  render() { 
+    return (
+      <div>
+        <h2>
+          {this.state.fizzBuzz || this.state.count}
+        </h2>
+        <div>
+          <button onClick={this.decrement.bind(this)}>Decrease</button>
+          <button onClick={this.increment.bind(this)}>Increase</button>
+        </div>
+      </div>
+    );
+  }
+  
+  fBuzz(num) {
+    if (num % 15 === 0) {
+      this.setState({fizzBuzz: 'FizzBuzz'});
+    } else if (num % 5 === 0) {
+      this.setState({fizzBuzz: 'Buzz'});
+    } else if (num % 3 === 0) {
+      this.setState({fizzBuzz: 'Fizz'});
+    } else {
+      this.setState({fizzBuzz: ''});
+    }
+  }
+  
+  decrement() {
+    if (this.state.count > 1) {
+      this.setState({count: --this.state.count});
+      this.fBuzz(this.state.count);
+    }
+  }
+  
+  increment() {
+    if (this.state.count < 100) {
+      this.setState({count: ++this.state.count});
+      this.fBuzz(this.state.count);
+    }
+  }
+}
+
+ReactDOM.render(
+	<FizzBuzz />, 
+	document.getElementById('root'));
